3e802bb66f2bc3bc81b6d35a680ebf79939d08d4
Anna37529 <matvey.grishenko@mail.ru>
2021-09-05 20:37:54 +0300

    нет 10 задачи

---
+++ JS_3.js
@@ -0,0 +1,32 @@
+/*let a = +prompt(`Введите число`);
+let i = 0;
+while (i < a){
+    i++,
+    document.write(` # `)
+}*/ 
+
+/*let num = +prompt(`Введите число`);
+let b = 0;
+while (b <= num){
+    document.write(`${num} `);
+    num--;
+}*/
+
+/*let number1 = +prompt(`Введите первое число`);
+let number2 = +prompt(`Введите второе число`);
+let n = number1 > number2 ? number2 : number1; 
+while (n >= 1){
+    if (number1 % n === 0){
+        if (number2 % n === 0){
+        document.write(`${n} `);   
+        }
+    }
+     n--;   
+}*/
+
+/*let num = +prompt(`Введите число`);
+let result = 1
+while (num != 0) {
+    result *= num;
+    num--;
+} document.write(result)*/
